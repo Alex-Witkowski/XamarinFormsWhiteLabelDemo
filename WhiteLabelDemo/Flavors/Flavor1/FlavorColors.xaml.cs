681a3c17b493f0692db3db1325ed131f6926e515
Alexander Witkowski <alexander.witkowski@develappers.de>
2019-06-14 13:26:55 +0200

    Moved colors to special resource dictionary for each flavor

---
+++ WhiteLabelDemo/Flavors/Flavor1/FlavorColors.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace WhiteLabelDemo.Flavors
+{
+    public partial class FlavorColors : ResourceDictionary
+    {
+        public FlavorColors()
+        {
+            InitializeComponent();
+        }
+    }
+}
